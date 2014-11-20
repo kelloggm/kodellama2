639afb13ff146a5da9da07f5ddce01b5e86165fb
Scelerus <scelerus@scelerus-1015E.(none)>
2014-11-20 16:45:09 -0500

    eval aexp

--- Aexp.v
+++ Aexp.v
@@ -7,6 +7,18 @@ Inductive AexpLit :=
   (* Lit has sign, numerator, and denominator *)
   | mk_aexp_lit : bool -> nat -> nat -> AexpLit.
 
+Definition plus_aexplit (a b : AexpLit) := mk_aexp_lit false 0 1.
+
+Definition minus_aexplit (a b : AexpLit) := mk_aexp_lit false 0 1.
+
+Definition mult_aexplit (a b : AexpLit) := mk_aexp_lit false 0 1.
+
+Definition div_aexplit (a b : AexpLit) := mk_aexp_lit false 0 1.
+
+Definition exp_aexplit (a b : AexpLit) := mk_aexp_lit false 0 1.
+
+Definition neg_aexplit (a : AexpLit) := mk_aexp_lit false 0 1.
+
 Inductive Aexp :=
   | Lit: AexpLit -> Aexp
   | Plus : Aexp -> Aexp -> Aexp
@@ -16,6 +28,16 @@ Inductive Aexp :=
   | Exp : Aexp -> Aexp -> Aexp
   | Neg : Aexp -> Aexp.
 
+Fixpoint eval_aexp (a : Aexp) :=
+  match a with
+    | Lit a' => a'
+    | Plus b c => plus_aexplit (eval_aexp b) (eval_aexp c)
+    | Minus b c => minus_aexplit (eval_aexp b) (eval_aexp c)
+    | Mult b c => mult_aexplit (eval_aexp b) (eval_aexp c)
+    | Div b c => div_aexplit (eval_aexp b) (eval_aexp c)
+    | Exp b c => exp_aexplit (eval_aexp b) (eval_aexp c)
+    | Neg b => neg_aexplit (eval_aexp b)
+  end.
 
 End Aexp.
 

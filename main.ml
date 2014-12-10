29d17044f878ea240b197b274af53c8fede863ec
Scelerus <scelerus@scelerus-1015E.(none)>
2014-12-09 21:55:19 -0500

    switches main to read in the string first and then pass it to the lexer. This should fix the super annoying eof thing

--- main.ml
+++ main.ml
@@ -5,8 +5,14 @@ open Printf
 let main () = 
   printf "Enter a Kodellama command:\n" ; 
   flush stdout ;
-  let lexbuf = Lexing.from_channel stdin in
-  let kdl_command = Parse.com Lex.initial lexbuf in
+  let stIn = ref "" in
+  try
+	while true do
+	      stIn := (!stIn ^ (input_line stdin))
+	done; 
+  with End_of_file -> () ;
+  let lexbuf = Lexing.from_string !stIn in
+  let kdl_command = (Parse.com Lex.initial lexbuf) in
   let sigma_0 = Sigma.Sigma.initial_state in 
   let sigma_n = Evals.Evals.eval_command kdl_command sigma_0 in
   ignore (sigma_n) ; 
